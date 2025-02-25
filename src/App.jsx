9b62aa002d50d5caa07c2260462c80adecfc8213
Vikas-Pendyala <153844036+Vikas-Pendyala@users.noreply.github.com>
2025-02-25 12:41:44 +0530

    completed landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import ShopByBrand from "./components/ShopByBrand/ShopByBrand";
 import ShopByCategories from "./components/ShopByCategories/ShopByCategories";
+import LandingPage from './components/LandingPage/LandingPage';
+import Products from "./components/Products/Products";
 
+function Layout() {
+  return (
+    <div>
+      <Navbar />
+      <Outlet /> {/* This renders the matched child route */}
+      <Footer />
+    </div>
+  );
+}
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <LandingPage /> }, // '/' will render <Hero/>
+      {
+        path: "/shop",
+        element: <ShopByBrand />,
+      },
+      {
+        path: "/cat",
+        element: <ShopByCategories/>,
+      },
+      {
+        path: "/products",
+        element: <Products/>
+      }
+    ],
+  },
+]);
 
 function App() {
-  return (
-  <div>
-    <Navbar/>
-    <Hero/>
-    <ShopByBrand/>
-    <ShopByCategories/>
-    <Footer/>
-  </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+import Hero from "../Hero/Hero"
+import ShopByBrand from "../ShopByBrand/ShopByBrand"
+import ShopByCategories from './../ShopByCategories/ShopByCategories';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Hero/>
+      <ShopByBrand/>
+      <ShopByCategories/>
+    </div>
+  )
+}
+
+export default LandingPage
